445e91e1c5d206b1d720f7f2a4e0393732cf85c6
Gopal-2025 <gopala.krishnan@techjose.com>
2026-07-18 14:28:20 +0530

    feeder file

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,18 +1,34 @@
-import { prisma } from "@/lib/prisma";
+import { PrismaClient } from "@prisma/client";
 
-await prisma.category.createMany({
-  data: [
-    {
-      name: "Electrician",
-      slug: "electrician",
-    },
-    {
-      name: "Plumber",
-      slug: "plumber",
-    },
-    {
-      name: "AC Repair",
-      slug: "ac-repair",
-    },
-  ],
-});
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.category.createMany({
+    data: [
+      {
+        name: "Electrician",
+        slug: "electrician",
+      },
+      {
+        name: "Plumber",
+        slug: "plumber",
+      },
+      {
+        name: "AC Repair",
+        slug: "ac-repair",
+      },
+    ],
+    skipDuplicates: true,
+  });
+
+  console.log("✅ Seed completed");
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
