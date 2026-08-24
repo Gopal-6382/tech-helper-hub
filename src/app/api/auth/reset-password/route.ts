aeb9652f8137fbb4217f8864705b69a493fc4b25
Gopal@6382 <gopalkrishnanto@gmail.com>
2026-08-24 08:50:14 +0530

    fix(auth): improve formatting and consistency in reset password route error handling

--- src/app/api/auth/reset-password/route.ts
+++ src/app/api/auth/reset-password/route.ts
@@ -4,13 +4,12 @@ import { resetPasswordAction } from "@/modules/auth/actions/reset-password.actio
 import { resetPasswordSchema } from "@/modules/auth/validations/auth.schema";
 
 export async function POST(req: NextRequest) {
-  try{
-  const body = await req.json();
-  const data = resetPasswordSchema.parse(body);
-  const result = await resetPasswordAction(data);
-  return successResponse(result);
-}   catch (error){
+  try {
+    const body = await req.json();
+    const data = resetPasswordSchema.parse(body);
+    const result = await resetPasswordAction(data);
+    return successResponse(result);
+  } catch (error) {
     return handleApiError(error);
-}
-
+  }
 }
