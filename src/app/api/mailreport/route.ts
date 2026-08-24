0e260b82ac4e06337ede46b0143bf84e4aefe958
Gopal@6382 <gopalkrishnanto@gmail.com>
2026-08-24 13:38:59 +0530

    feat(mailreport): refactor POST route to use routeHandler and remove authMiddleware

--- src/app/api/mailreport/route.ts
+++ src/app/api/mailreport/route.ts
@@ -1,16 +1,16 @@
-import { NextRequest } from "next/server";
-
-import { authMiddleware } from "@/middleware/auth.middleware";
-import { handleRequest } from "@/utils/api.helper";
-
+import { routeHandler } from "@/middleware/route.handler";
 import { createIssueReport } from "@/modules/mailreports/actions/createIssueReport.action";
-
 import { CreateIssueReportDto } from "@/modules/mailreports/types/issue-report.types";
+import { USER_ROLES } from "@/constant/role.constant";
+import { createIssueReportSchema } from "@/modules/mailreports/validations/issue-report.validations";
 
-export const POST = authMiddleware(async (req: NextRequest, user) => {
-  return handleRequest(async () => {
+export const POST = routeHandler(
+  async (req, user) => {
     const body: CreateIssueReportDto = await req.json();
-
-    return createIssueReport(user.userId, body);
-  });
-});
+const data=createIssueReportSchema.parse(body);
+    return createIssueReport(user.userId, data);
+  },
+  {
+    roles: USER_ROLES,
+  }
+);
