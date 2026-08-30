8bdd6b568fe7d8cb8e628ad1c58a30f80973109e
Gopal@6382 <gopalkrishnanto@gmail.com>
2026-08-30 15:28:37 +0530

    refactor(saveposts): streamline route handlers and role management for GET and POST endpoints

--- src/app/api/saveposts/[postId]/route.ts
+++ src/app/api/saveposts/[postId]/route.ts
@@ -1,6 +1,8 @@
 import { routeHandler } from "@/middleware/route.handler";
 import { unsavePost } from "@/modules/saveposts/actions/unsave-post.action";
-import { USER_ROLES } from "@/constant/role.constant";
+import { savePost } from "@/modules/saveposts/actions/save-post.action";
+import { createSavedPostSchema } from "@/modules/saveposts/validations/saved-post.validation";
+import { User } from "@/constant/roles.route.const";
 
 type SavedPostRouteParams = {
   postId: string;
@@ -17,7 +19,16 @@ export const DELETE = routeHandler<SavedPostRouteParams>(
 
     return unsavePost(user.userId, postId);
   },
-  {
-    roles: USER_ROLES,
+  User,
+);
+
+// POST /api/saved-posts
+export const POST = routeHandler<SavedPostRouteParams>(
+  async (_req, user, { params }) => {
+    const body = await params;
+    const data = createSavedPostSchema.parse(body);
+
+    return savePost(user.userId, data);
   },
+  User,
 );
--- src/app/api/saveposts/route.ts
+++ src/app/api/saveposts/route.ts
@@ -1,28 +1,8 @@
+import { User } from "@/constant/roles.route.const";
 import { routeHandler } from "@/middleware/route.handler";
 import { getSavedPosts } from "@/modules/saveposts/actions/get-saved-posts.action";
-import { savePost } from "@/modules/saveposts/actions/save-post.action";
-import { createSavedPostSchema } from "@/modules/saveposts/validations/saved-post.validation";
-import { USER_ROLES } from "@/constant/role.constant";
 
 // GET /api/saved-posts
-export const GET = routeHandler(
-  async (_req, user) => {
-    return getSavedPosts(user.userId);
-  },
-  {
-    roles: USER_ROLES,
-  },
-);
-
-// POST /api/saved-posts
-export const POST = routeHandler(
-  async (req, user) => {
-    const body = await req.json();
-    const data = createSavedPostSchema.parse(body);
-
-    return savePost(user.userId, data);
-  },
-  {
-    roles: USER_ROLES,
-  },
-);
+export const GET = routeHandler(async (_req, user) => {
+  return getSavedPosts(user.userId);
+}, User);
